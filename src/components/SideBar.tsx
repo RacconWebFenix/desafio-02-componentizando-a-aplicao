baccf8106ea6d9782817696713456281b29a6868
Marcos Domingues <marcos.domingues@gcbinvestimentos.com>
2022-04-25 17:35:15 -0300

    SideBar component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,37 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+}
+
+interface DataProps {
+  data: GenreResponseProps[];
+  selectId: number;
+  clickEvent: (id: number) => void;
+}
+
+export function SideBar({ data, selectId, clickEvent }: DataProps) {
+  return (
+    <>
+      <nav className="sidebar">
+        <span>
+          Watch<p>Me</p>
+        </span>
+
+        <div className="buttons-container">
+          {data.map((d) => (
+            <Button
+              key={String(d.id)}
+              title={d.title}
+              iconName={d.name}
+              onClick={() => clickEvent(d.id)}
+              selected={selectId === d.id}
+            />
+          ))}
+        </div>
+      </nav>
+    </>
+  );
 }
